fe6c06f29d7a6e05a7b77038b869ebf27f1cbf3f
RonyAndres <ronyandres181285@gmail.com>
2022-06-17 22:51:56 -0500

    Etiqueta

--- src/clases/Producto.java
+++ src/clases/Producto.java
@@ -1,5 +1,25 @@
 package clases;
 
 public class Producto {
-
+	private String codigo;
+	private String descripcion;
+	private String categoria;
+	private int cantidad;
+	private double precio;
+	private double costo;
+	
+	Producto(String codigo,String descripcion,String categoria){
+		this.codigo=codigo;
+		this.descripcion=descripcion;
+		this.categoria=categoria;
+		cantidad=0;
+		precio=0;
+		costo=0;
+	}
+	
+	Producto(String codigo,String descripcion){
+		this(codigo,descripcion,"Varios");
+	}
+	
+	
 }
